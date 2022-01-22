129144c1b4e3e380425144eaa9f1ae0803751518
tharshith <jtharshith@gmail.com>
2022-01-22 05:53:50 +0530

    changes to mobile landscape nav-3

--- src/Components/Other/Navbar/HamburgerMenu.component.jsx
+++ src/Components/Other/Navbar/HamburgerMenu.component.jsx
@@ -16,52 +16,44 @@ const HamburgerMenu = () => {
         }
       >
         <Link to="/" className="nav-link-black">
-        <li
-          className="underline nav-underline-red"
-          style={{ animation: "navLinkFade 0.4s ease forwards 0.15s" }}
-        >
-          <span>
-            Home
-          </span>
-        </li>
+          <li
+            className="underline nav-underline-red"
+            style={{ animation: "navLinkFade 0.4s ease forwards 0.15s" }}
+          >
+            <span onClick={menuShow}>Home</span>
+          </li>
         </Link>
         <Link to="/about" className="nav-link-black">
-        <li
-          className="underline nav-underline-green"
-          style={{ animation: "navLinkFade 0.4s ease forwards 0.3s" }}
-        >
-          <span>
-            About
-          </span>
-        </li>
+          <li
+            className="underline nav-underline-green"
+            style={{ animation: "navLinkFade 0.4s ease forwards 0.3s" }}
+          >
+            <span onClick={menuShow}>About</span>
+          </li>
         </Link>
         <Link to="/projects" className="nav-link-black">
-        <li
-          className="underline nav-underline-blue"
-          style={{ animation: "navLinkFade 0.4s ease forwards 0.45s" }}
-        >
-          <span>
-            Projects
-          </span>
-        </li>
+          <li
+            className="underline nav-underline-blue"
+            style={{ animation: "navLinkFade 0.4s ease forwards 0.45s" }}
+          >
+            <span onClick={menuShow}>Projects</span>
+          </li>
         </Link>
         <Link to="/timeline" className="nav-link-black">
-        <li
-          className="underline nav-underline-yellow"
-          style={{ animation: "navLinkFade 0.4s ease forwards 0.6s" }}
-        >
-          <span>
-            Timeline
-          </span>
-        </li>
+          <li
+            className="underline nav-underline-yellow"
+            style={{ animation: "navLinkFade 0.4s ease forwards 0.6s" }}
+          >
+            <span onClick={menuShow}>Timeline</span>
+          </li>
         </Link>
         <Link to="/contact" className="nav-link-black">
-        <li
-          className="word-break"
-          style={{ animation: "navLinkFade 0.5s ease forwards 0.75s" }}
-        >
-          Contact Us
-        </li>
+          <li
+            className="word-break"
+            style={{ animation: "navLinkFade 0.5s ease forwards 0.75s" }}
+          >
+            <span onClick={menuShow}>Contact Us</span>
+          </li>
         </Link>
       </ul>
       <button onClick={menuShow}>
