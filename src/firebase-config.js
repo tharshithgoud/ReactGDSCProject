bb2c16774e4266eea15261c82e5d39a9064ce066
Tharshtih Goud <69805722+tharshithgoud@users.noreply.github.com>
2022-10-21 18:02:48 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC79-xZfBCErxeZwzDCxPDFYKhWnrSD22Q",
-  authDomain: "gdscwebsite-c3449.firebaseapp.com",
-  projectId: "gdscwebsite-c3449",
-  storageBucket: "gdscwebsite-c3449.appspot.com",
-  messagingSenderId: "111698852663",
-  appId: "1:111698852663:web:6ec58b0196b5a0025578b2",
 };
 
 const app = initializeApp(firebaseConfig);
